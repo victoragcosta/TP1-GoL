75bda5b392dde151672ee20b3393180db9b00c5b
Hugo-NF <hugonfonseca@hotmail.com>
2017-05-12 10:26:02 -0300

    Estrutura Inicial

---
+++ gol-Strategy/src/main/scala/Cell.scala
@@ -0,0 +1,14 @@
+/**
+ * Rerepsentacao de uma celula do GoL 
+ * 
+ * @author Breno Xavier (baseado na implementacao Java de rbonifacio@unb.br
+ */
+class Cell {
+  
+  private var alive = false
+  
+  def isAlive = alive
+  
+  def kill = alive = false
+  def revive = alive = true
+}
---
+++ gol-Template_method/build.sbt
@@ -0,0 +1,6 @@
+name := "gol-Template_method"
+
+version := "1.0"
+
+scalaVersion := "2.12.2"
+        
