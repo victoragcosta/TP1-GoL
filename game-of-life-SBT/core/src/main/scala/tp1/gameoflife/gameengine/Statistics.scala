b770ceea4687cb73482b3ead6460bfecb2bf6468
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2017-05-15 21:27:10 -0300

    Reinserção de método para imprimir estatísticas.

--- game-of-life-SBT/core/src/main/scala/tp1/gameoflife/gameengine/Statistics.scala
+++ game-of-life-SBT/core/src/main/scala/tp1/gameoflife/gameengine/Statistics.scala
@@ -11,4 +11,15 @@ object Statistics {
   def showDeaths(): Int = numberOfDeaths
   def addDeath(): Unit = numberOfDeaths += 1
 
+  def displayStatistics(): Unit = {
+    println("                                     ")
+    println("-------------------------------------")
+    println("             Statistics              ")
+    println("-------------------------------------")
+    println("Number of revives: " + numberOfRevives)
+    println("Number of deaths: " + numberOfDeaths)
+    println("-------------------------------------")
+    println("                                     ")
+  }
+
 }
